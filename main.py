23bfc010142143ce6a169a890fcd155711cc7e7d
Oleksandr Piatnytsia <a_pyatnytsia@viyar.ua>
2026-05-17 20:12:54 +0300

    string def

---
+++ main.py
@@ -0,0 +1,12 @@
+def get_str_length(inc_string: str) -> int:
+    return len(inc_string)
+
+
+def concat_strings(string1: str, string2: str) -> str:
+    return string1 + string2
+
+
+if __name__ == "__main__":
+    print(get_str_length("dfgh"))
+
+    print(concat_strings("asd", "fgh"))
